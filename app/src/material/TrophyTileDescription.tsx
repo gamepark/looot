acf1854a75258d54d7c52c125eb31be5f31cbc06
Romain Fromi <romain.fromi@gmail.com>
2025-05-31 10:12:47 +0200

    Fix Trophy tiles rotation

--- app/src/material/TrophyTileDescription.tsx
+++ app/src/material/TrophyTileDescription.tsx
@@ -1,31 +1,32 @@
 /** @jsxImportSource @emotion/react */
+import { LocationType } from '@gamepark/looot/material/LocationType'
 import { Trophy } from '@gamepark/looot/material/Trophy'
 import { CardDescription } from '@gamepark/react-game'
 import { MaterialItem } from '@gamepark/rules-api'
-import Trophy2 from '../images/trophees/trophee2.png'
-import Trophy3 from '../images/trophees/trophee3.png'
-import Trophy4 from '../images/trophees/trophee4.png'
-import Trophy5 from '../images/trophees/trophee5.png'
-import Trophy6 from '../images/trophees/trophee6.png'
 import Back2 from '../images/trophees/back2.png'
 import Back3 from '../images/trophees/back3.png'
 import Back4 from '../images/trophees/back4.png'
 import Back5 from '../images/trophees/back5.png'
 import Back6 from '../images/trophees/back6.png'
+import Trophy2 from '../images/trophees/trophee2.png'
+import Trophy3 from '../images/trophees/trophee3.png'
+import Trophy4 from '../images/trophees/trophee4.png'
+import Trophy5 from '../images/trophees/trophee5.png'
+import Trophy6 from '../images/trophees/trophee6.png'
 
 export class TrophyTileDescription extends CardDescription {
   height = 2.7
   width = 3
 
-  backImages = backImages
   images = images
+  backImages = backImages
 
-  isFlipped(item: Partial<MaterialItem>): boolean {
-    return item.location?.rotation as boolean
+  isFlipped(item: Partial<MaterialItem>) {
+    return item.location?.type === LocationType.FjordBoardHexSpace
   }
 }
 
-const backImages = {
+const images = {
   [Trophy.Trophy2]: Back2,
   [Trophy.Trophy3]: Back3,
   [Trophy.Trophy4]: Back4,
@@ -33,7 +34,7 @@ const backImages = {
   [Trophy.Trophy6]: Back6
 }
 
-const images = {
+const backImages = {
   [Trophy.Trophy2]: Trophy2,
   [Trophy.Trophy3]: Trophy3,
   [Trophy.Trophy4]: Trophy4,
