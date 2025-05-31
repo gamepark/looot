f46de9edb013401016e88dc2d6949fed507240f5
Romain Fromi <romain.fromi@gmail.com>
2025-05-31 11:06:17 +0200

    Center the landscape on the table

--- app/src/locators/LandscapeLocator.ts
+++ app/src/locators/LandscapeLocator.ts
@@ -1,11 +1,20 @@
 /** @jsxImportSource @emotion/react */
 import { MaterialType } from '@gamepark/looot/material/MaterialType'
+import { LandscapeHelper } from '@gamepark/looot/rules/helpers/LandscapeHelper'
 import { HexagonalGridLocator, ItemContext } from '@gamepark/react-game'
-import { HexGridSystem, MaterialItem } from '@gamepark/rules-api'
+import { MaterialContext } from '@gamepark/react-game/dist/locators/Locator'
+import { HexGridSystem, Location, MaterialItem } from '@gamepark/rules-api'
 
 class LandscapeLocator extends HexagonalGridLocator {
   coordinatesSystem = HexGridSystem.EvenQ
   size = 1.92
+
+  getCoordinates(_: Location, { rules }: MaterialContext) {
+    const helper = new LandscapeHelper(rules.game)
+    const x = -(helper.xMin + helper.xMax) / 2
+    const y = -(helper.yMin + helper.yMax) / 2
+    return this.getHexagonPosition({ x, y })
+  }
 
   getItemCoordinates(item: MaterialItem, context: ItemContext) {
     const { x = 0, y = 0 } = super.getItemCoordinates(item, context)
@@ -15,7 +24,7 @@ class LandscapeLocator extends HexagonalGridLocator {
       return {
         x: x - context.displayIndex * 0.07,
         y: y - context.displayIndex * 0.1,
-        z: z + context.displayIndex * 0.1,
+        z: z + context.displayIndex * 0.1
       }
     }
     return { x, y, z }
