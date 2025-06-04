ba4cd42eee20a1b5e663d91fb920527aa796e590
Romain Fromi <romain.fromi@gmail.com>
2025-06-04 14:15:01 +0200

    Only include one Longship in the dom

--- app/src/locators/longshipsPileLocator.ts
+++ app/src/locators/longshipsPileLocator.ts
@@ -1,10 +1,12 @@
 /** @jsxImportSource @emotion/react */
-import { PileLocator } from '@gamepark/react-game'
+import { Locator } from '@gamepark/react-game'
 import { MaterialContext } from '@gamepark/react-game/dist/locators/Locator'
 import { Location } from '@gamepark/rules-api'
 import { bagLocator } from './BagLocator'
 
-class LongshipsPileLocator extends PileLocator {
+class LongshipsPileLocator extends Locator {
+  limit = 1
+
   getCoordinates(location: Location, context: MaterialContext) {
     return bagLocator.getCoordinates(location, context)
   }
