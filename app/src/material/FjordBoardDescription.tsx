6ac6e567e0fbd2a7d4349b8c4571fc0c23054aa4
sylvestredavid <david.sylvestre.24@gmail.com>
2025-06-05 14:40:11 +0200

    plateaux fjord en jpg

--- app/src/material/FjordBoardDescription.tsx
+++ app/src/material/FjordBoardDescription.tsx
@@ -1,15 +1,16 @@
 /** @jsxImportSource @emotion/react */
 import { PlayerColor } from '@gamepark/looot/PlayerColor'
 import { BoardDescription } from '@gamepark/react-game'
-import BlueFjord from '../images/plateaux/fjord_bleu.png'
-import RedFjord from '../images/plateaux/fjord_rouge.png'
-import GreyFjord from '../images/plateaux/fjord_gris.png'
-import YellowFjord from '../images/plateaux/fjord_jaune.png'
+import BlueFjord from '../images/plateaux/fjord_bleu.jpg'
+import RedFjord from '../images/plateaux/fjord_rouge.jpg'
+import GreyFjord from '../images/plateaux/fjord_gris.jpg'
+import YellowFjord from '../images/plateaux/fjord_jaune.jpg'
 import { FjordBoardHelp } from './help/FjordBoardHelp'
 
 export class FjordBoardDescription extends BoardDescription {
   height = 24
   width = 24
+  borderRadius = 0.8
 
   images = {
     [PlayerColor.Blue]: BlueFjord,
