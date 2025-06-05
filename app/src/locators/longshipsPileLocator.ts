989376b2557e60db93bd5331ec2c36700da0f6dd
sylvestredavid <david.sylvestre.24@gmail.com>
2025-06-05 13:52:21 +0200

    fix bag

--- app/src/locators/longshipsPileLocator.ts
+++ app/src/locators/longshipsPileLocator.ts
@@ -1,7 +1,7 @@
 /** @jsxImportSource @emotion/react */
 import { Locator } from '@gamepark/react-game'
-import { MaterialContext } from '@gamepark/react-game/dist/locators/Locator'
-import { Location } from '@gamepark/rules-api'
+import { ItemContext, MaterialContext } from '@gamepark/react-game/dist/locators/Locator'
+import { Coordinates, Location, MaterialItem } from '@gamepark/rules-api'
 import { bagLocator } from './BagLocator'
 
 class LongshipsPileLocator extends Locator {
@@ -9,6 +9,10 @@ class LongshipsPileLocator extends Locator {
 
   getCoordinates(location: Location, context: MaterialContext) {
     return bagLocator.getCoordinates(location, context)
+  }
+  getItemCoordinates(item: MaterialItem, context: ItemContext): Partial<Coordinates> {
+    const { x, y } = super.getItemCoordinates(item, context)
+    return { x, y, z: -1 }
   }
 }
 
