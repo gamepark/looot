d868514d433b40009918ec290463de1a39c81c99
sylvestredavid <david.sylvestre.24@gmail.com>
2025-06-10 22:16:44 +0200

    rappel scoring popup d'aide plateau fjord

--- app/src/material/help/FjordBoardHelp.tsx
+++ app/src/material/help/FjordBoardHelp.tsx
@@ -17,6 +17,42 @@ export const FjordBoardHelp: FC<MaterialHelpProps> = () => {
       <p>
         <Trans defaults={`help.fjord.descr`} components={components} />
       </p>
+      <h2>{t(`help.fjord.scoring`)}</h2>
+      <ul>
+        <li>
+          <Trans defaults={`help.fjord.scoring.wood`} components={components} />
+        </li>
+        <li>
+          <Trans defaults={`help.fjord.scoring.sheep`} components={components} />
+        </li>
+        <li>
+          <Trans defaults={`help.fjord.scoring.gold`} components={components} />
+        </li>
+        <li>
+          <Trans defaults={`help.fjord.scoring.house`} components={components} />
+        </li>
+        <li>
+          <Trans defaults={`help.fjord.scoring.watchtower`} components={components} />
+        </li>
+        <li>
+          <Trans defaults={`help.fjord.scoring.castle`} components={components} />
+        </li>
+        <li>
+          <Trans defaults={`help.fjord.scoring.seaport`} components={components} />
+        </li>
+        <li>
+          <Trans defaults={`help.fjord.scoring.altar`} components={components} />
+        </li>
+        <li>
+          <Trans defaults={`help.fjord.scoring.jarl`} components={components} />
+        </li>
+        <li>
+          <Trans defaults={`help.fjord.scoring.trophy`} components={components} />
+        </li>
+        <li>
+          <Trans defaults={`help.fjord.scoring.drakkar`} components={components} />
+        </li>
+      </ul>
     </>
   )
 }
